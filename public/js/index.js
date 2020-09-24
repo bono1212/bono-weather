170099e311cb66136c478224a459f315ac03b000
bono1212 <uyanga1212g@gmail.com>
2020-09-24 11:26:04 +0900

    ff

---
+++ public/js/index.js
@@ -0,0 +1,8 @@
+/* ********************전역 설정********************** */
+var appid = 'b059e459e8e4a4f9eee7a5505a60b714';
+var dailyURL = 'http://api.openweathermap.org/data/2.5/weather';
+var weeklyURL = 'http://api.openweathermap.org/data/2.5/forecast';
+var sendData = { units: 'metric', lang: 'kr', appid: appid }
+
+console.log(sendData.appid);
+
