9bae183d2b34c9037d90c705f0f41f12b48deb01
bono1212 <uyanga1212g@gmail.com>
2020-09-29 14:31:36 +0900

    hh

--- public/js/youtube.js
+++ public/js/youtube.js
@@ -1,16 +1,9 @@
 $("#video").YTPlayer();
 
-
-
-/* 
-/* $(".video-cover").mouseover(function(e){
-	e.stopPropagation(e);
-}); */
-
 /* setTimeout(function(){
 	$(".video").css("transform", "scale(1)");
 }, 5000);
 
-$(".video-cover").
-
- */ 
+$(".video-cover").mouseover(function(e){
+	e.stopPropagation();
+}); */
