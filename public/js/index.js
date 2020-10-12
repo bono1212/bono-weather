1edc33c488d41a65b0570393fdb5547b31e370f0
bono1212 <uyanga1212g@gmail.com>
2020-10-12 11:47:54 +0900

    gg

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,4 @@
 /******************* 전역설정 ********************/
-var kakaoKey = '644d617a85cb7f28982d642138d22c33';
 var appid = '02efdd64bdc14b279bc91d9247db4722';
 var dailyURL = 'https://api.openweathermap.org/data/2.5/weather';
 var weeklyURL = 'https://api.openweathermap.org/data/2.5/forecast';
