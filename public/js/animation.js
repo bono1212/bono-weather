37277c480cb3a030c34c461ccc41842862818b7e
bono1212 <uyanga1212g@gmail.com>
2020-10-12 11:38:18 +0900

    vv

--- public/js/animation.js
+++
@@ -1,8 +0,0 @@
-$(".box1").click(function(){
-	if(	$(this).css("animation-play-state") == 'paused'	){
-		$(this).css("animation-play-state", "running");
-	}
-	else {
-		$(this).css("animation-play-state", "paused");
-	}
-});
